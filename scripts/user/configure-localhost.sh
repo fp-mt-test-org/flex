e35dcd7c227d34af24750b03bc29aa9b7e8144e2
Matthew Thomas <matthewthomas@flexport.com>
2021-06-22 16:28:24 -0700

    fix: Gets the logic actually working.

--- scripts/user/configure-localhost.sh
+++ scripts/user/configure-localhost.sh
@@ -5,15 +5,18 @@ set -o pipefail
 set -o nounset
 
 case "$SHELL" in
- "/bin/zsh") profile_path=~/.zshrc ;;
- *) profile_path=~/.bashrc ;;
+ "/bin/zsh") profile_path=bha ;;
+ *) profile_path=~/.zshrc ;;
 esac
 
 script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 flex_alias=$(cat "${script_path}/configure-alias.sh")
 
-if ! grep -q "${flex_alias}" "${profile_path}"; then
-    profile_content=$(cat ${profile_path}) 2>/dev/null
+if ! grep -q "${flex_alias}" "${profile_path}" 2>/dev/null || true; then
+    echo "Gonna write!"
+    profile_content=$(cat ${profile_path} 2>/dev/null || true) 
+
+
     # Save it to the profile so it's execute for each shell session.
     echo -e "${flex_alias}\n${profile_content}" > "${profile_path}"
 
