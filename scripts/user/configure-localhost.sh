24d4018ba9d1c8be0f53faf5a75002c29c147dee
Matthew Thomas <matthewthomas@flexport.com>
2021-06-22 17:53:10 -0700

    fix: Better implementation

--- scripts/user/configure-localhost.sh
+++ scripts/user/configure-localhost.sh
@@ -9,18 +9,26 @@ case "$SHELL" in
  *) profile_path=~/.zshrc ;;
 esac
 
-script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 flex_alias=$(cat "${script_path}/configure-alias.sh")
 
-if ! grep -q "${flex_alias}" "${profile_path}" 2>/dev/null || true; then
-    profile_content=$(cat ${profile_path} 2>/dev/null || true) 
+update_profile() {
+    profile_path="${1}"
+    flex_alias="${2}"
 
-    # Save it to the profile so it's execute for each shell session.
     echo -e "${flex_alias}\n${profile_content}" > "${profile_path}"
-
     echo "Added ${flex_alias} to your ${profile_path}"
 
     # Start a new shell session so that the
     # user can use the alias immediately.
     $SHELL
+}
+
+if [[ -f "${profile_path}" ]]; then
+    if ! grep -q "${flex_alias}" "${profile_path}" ; then
+        profile_content=$(cat "${profile_path}") 
+        update_profile "${profile_path}" "${flex_alias}"
+    fi
+else
+    update_profile "${profile_path}" "${flex_alias}"
 fi
